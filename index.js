55e719ac5bb79c1eea4520174aaed12afb5f2f0a
Your Name <william.gruszka@gmail.com>
2022-09-15 15:37:47 +1000

    separated algorithms into their own functions

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-// varibel setup
-let colourPallete = [];
-let palleteLen = 0;
-let size = 0;
-
-//get HTML canvas to draw to later
 const c = document.getElementById("myCanvas");
 const ctx = c.getContext("2d");
+let colourpalette = [];
+let paletteLen = 0;
+let size = 0;
 
 // define rgb class
 class rgb {
@@ -17,50 +14,53 @@ class rgb {
 }
 
 // create array of colours
-for (let iBlue = 8; iBlue <= 256; iBlue += 8) {
-    for (let iGreen = 8; iGreen <= 256; iGreen += 8) {
-        for (let iRed = 8; iRed <= 256; iRed += 8) {
-            const newColour = new rgb(iRed, iGreen, iBlue);
-            colourPallete.push(newColour);
+const generatePalette = () => {
+    colourpalette = [];
+
+    // loop over each colour channel in increments of 8
+    for (let iBlue = 8; iBlue <= 256; iBlue += 8) {
+        for (let iGreen = 8; iGreen <= 256; iGreen += 8) {
+            for (let iRed = 8; iRed <= 256; iRed += 8) {
+                const newColour = new rgb(iRed, iGreen, iBlue);
+                colourpalette.push(newColour);
+            }
         }
     }
+
+    return colourpalette
 }
 
-// get number of colours and the size of a square that would fit all colours
-palleteLen = colourPallete.length;
-size = Math.round(Math.sqrt(palleteLen) / 100) * 100;
-
-// render a randomly generated noise image that only uses all colours and only once
-const renderImage = () => {
+// render a randomly generated noise image that uses all colours only once
+const renderImage = (colourpalette) => {
     let PositionIndex = 0;
     let colourIndex = 0;
     let usedColours = [];
 
+    // get number of colours and the size of a square that would fit all the colours as pixels
+    paletteLen = colourpalette.length;
+    size = Math.round(Math.sqrt(paletteLen) / 100) * 100;
+
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
 
-            // if PositionIndex has reached the length of the colour pallete, then stop rendering. 
-            // As there are not enough pixels to fill a perfect square, the loop would never really 
-            // fill up without repeating pixels.
-            if (PositionIndex >= palleteLen) {
+            // if PositionIndex has reached the length of the colour palette, then stop rendering
+            if (PositionIndex >= paletteLen) {
                 break;
             }
 
-            // get colourIndex for next colour randomly. If it has already been used, keep trying until
-            // a unique pixel has been generated
+            // generate random colourIndex for next colour until a unique one has been generated
             do {
-                colourIndex = Math.floor(Math.random() * ((palleteLen)))
+                colourIndex = Math.floor(Math.random() * ((paletteLen)))
             } while (usedColours.includes(colourIndex))
 
-            // add colourIndex to array of used colours
             usedColours.push(colourIndex);
             
             // draw the pixel to the canvas
             ctx.fillStyle = 
                 `rgb(
-                    ${colourPallete[colourIndex]?.red}, 
-                    ${colourPallete[colourIndex]?.green}, 
-                    ${colourPallete[colourIndex]?.blue}
+                    ${colourpalette[colourIndex]?.red}, 
+                    ${colourpalette[colourIndex]?.green}, 
+                    ${colourpalette[colourIndex]?.blue}
                 )`;
             ctx.fillRect(j * 4, i * 4, 4, 4);
     
@@ -70,6 +70,7 @@ const renderImage = () => {
 }
 
 // add event listener to the HTML button that will call the renderImage function.
-document.getElementById('render-btn').addEventListener('click', ()=>{
-    renderImage();
+document.getElementById('render-btn').addEventListener('click', () => {
+    const cp = generatePalette();
+    renderImage(cp);
 })
